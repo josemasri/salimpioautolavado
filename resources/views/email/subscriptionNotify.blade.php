3e9797280df57c7d37edbc56a703d65754bedb05
Jose Masri <josemasri2@gmail.com>
2022-10-03 09:46:40 -0500

    Bug fix

--- resources/views/email/subscriptionNotify.blade.php
+++ resources/views/email/subscriptionNotify.blade.php
@@ -16,7 +16,7 @@
       <br>
       <p class="message-title">{{ $messageContent }} </p>
       <div class="message-info">
-        <p>Residencial: Palmas Hills</p>
+        <p>Residencial: Puerta del sol</p>
         <p>Información del cliente</p>
         <p>Nombre: {{ $customer->name  }} {{ $customer->last_name }}</p>
         <p>Teléfono: {{ $customer->phone  }}</p>
